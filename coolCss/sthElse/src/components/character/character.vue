02e799ad113a09d81b8e8e7470e589146afb4765
lemonteatree <ldflvdongfeng@hotmail.com>
2021-02-19 18:05:49 +0800

    --feat:完善物品栏点击判定,重构小人

--- coolCss/sthElse/src/components/character/character.vue
+++ coolCss/sthElse/src/components/character/character.vue
@@ -1,29 +1,42 @@
 <template>
-  <div id="character">
-    <div class="character" v-for="(item,index) in characterShadows" :key="index" :style="'animation-delay:'+ (index*index)/30 +'s;opacity:'+ (1-(index/5)) +';z-index:'+(6-index)">
-      <div class="head" :style="'animation-delay:0.'+ index +'s;'"></div>
-      <div class="bodyTop" :style="'animation-delay:0.'+ index +'s;'"></div>
-      <div class="bodyBottom" :style="'animation-delay:0.'+ index +'s;'"></div>
-      <div class="armLeftFront" :style="'animation-delay:0.'+ index +'s;'"></div>
-      <div class="armLeftBehind" :style="'animation-delay:0.'+ index +'s;'"></div>
-      <div class="armRightFront" :style="'animation-delay:0.'+ index +'s;'"></div>
-      <div class="armRightBehind" :style="'animation-delay:0.'+ index +'s;'"></div>
-      <div class="legLeftFront" :style="'animation-delay:0.'+ index +'s;'"></div>
-      <div class="legLeftBehind" :style="'animation-delay:0.'+ index +'s;'"></div>
-      <div class="legRightFront" :style="'animation-delay:0.'+ index +'s;'"></div>
-      <div class="legRightBehind" :style="'animation-delay:0.'+ index +'s;'"></div>
+    <div id="character">
+        <div class="character" v-for="(item, index) in characterShadows" :key="index" :style="
+                'animation-delay:' +
+                (index * index) / 30 +
+                's;opacity:' +
+                (1 - index / 5) +
+                ';z-index:' +
+                (6 - index)
+            ">
+            <div class="legRightBehind" :style="'animation-delay:0.' + index + 's;'">
+                <div class="legRightFront" :style="'animation-delay:0.' + index + 's;'"></div>
+                <div class="legLeftBehind" :style="'animation-delay:0.' + index + 's;'">
+                    <div class="legLeftFront" :style="'animation-delay:0.' + index + 's;'"></div>
+                </div>
+                <div class="bodyBottom" :style="'animation-delay:0.' + index + 's;'">
+                    <div class="bodyTop" :style="'animation-delay:0.' + index + 's;'">
+                        <div class="armRightBehind" :style="'animation-delay:0.' + index + 's;'">
+                            <div class="armRightFront" :style="'animation-delay:0.' + index + 's;'"></div>
+                        </div>
+                        <div class="armLeftBehind" :style="'animation-delay:0.' + index + 's;'">
+                            <div class="armLeftFront" :style="'animation-delay:0.' + index + 's;'"></div>
+                        </div>
+                        <div class="head" :style="'animation-delay:0.' + index + 's;'"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      characterShadows:[1,2,3,4,5]
-    }
-  }
-}
+    data() {
+        return {
+            characterShadows: [1, 2, 3, 4, 5],
+        };
+    },
+};
 </script>
 
 <style lang="less" scoped>
