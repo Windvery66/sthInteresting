68f8b97e70f7eda74b5e125c30b62747f3d6aeab
lemonteatree <ldflvdongfeng@hotmail.com>
2021-03-11 18:02:08 +0800

    --feat:添加战斗进度条

--- coolCss/sthElse/src/views/test2/test2.vue
+++ coolCss/sthElse/src/views/test2/test2.vue
@@ -18,8 +18,8 @@
                 </div>
                 <!-- 我方血蓝数值 -->
                 <div class="playerValue numberValue">
-                  <div class="hpValue">HP:{{playerCurrentHp}}</div>
-                  <div class="mpValue">MP:{{playerCurrentMp}}</div>
+                    <div class="hpValue">HP:{{playerCurrentHp}}</div>
+                    <div class="mpValue">MP:{{playerCurrentMp}}</div>
                 </div>
                 <!-- VS -->
                 <div class="vs">VS</div>
@@ -38,14 +38,41 @@
                 </div>
                 <!-- 敌方血蓝数值 -->
                 <div class="enemyValue numberValue">
-                  <div class="mpValue">MP:{{playerCurrentMp}}</div>
-                  <div class="hpValue">HP:{{playerCurrentHp}}</div>
+                    <div class="mpValue">MP:{{enemyCurrentMp}}</div>
+                    <div class="hpValue">HP:{{enemyCurrentHp}}</div>
                 </div>
             </div>
             <!-- 主要部分(双方人物,战斗动画) -->
             <div class="battleAreaMain"></div>
             <!-- 进度条(速度条) -->
-            <div class="progressBar"></div>
+            <div class="progressBars">
+                <!-- 我方进度条 -->
+                <div class="progressBar player">
+                    <div class="progressBarBg"></div>
+                    <div class="innerProgressBar">
+                        <div class="progressBarItems" :style="'transform:translateX('+(runTime/10)+'rem)'">
+                            <div class="progressBarItem" v-for="(item,index) in playerProgress" :key="index">
+                                <div class="afterAnimation" v-show="item.afterAnimation" :style="'width:'+item.afterAnimation+'rem'"></div>
+                                <img :src="item.img" alt="">
+                                <div class="attackAnimation" v-show="item.attackAnimation" :style="'width:'+item.attackAnimation+'rem'"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- 敌方进度条 -->
+                <div class="progressBar enemy">
+                    <div class="progressBarBg"></div>
+                    <div class="innerProgressBar">
+                        <div class="progressBarItems" :style="'transform:translateX(-'+(runTime/10)+'rem)'">
+                            <div class="progressBarItem" v-for="(item,index) in enemyProgress" :key="index">
+                                <div class="attackAnimation" v-show="item.attackAnimation" :style="'width:'+item.attackAnimation+'rem'"></div>
+                                <img :src="item.img" alt="">
+                                <div class="afterAnimation" v-show="item.afterAnimation" :style="'width:'+item.afterAnimation+'rem'"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <!-- 技能池 -->
             <div class="skillPool"></div>
 
@@ -58,6 +85,8 @@
 export default {
     data() {
         return {
+            runTime: 0,
+            fightRun: null,
             playerCurrentHp: 1000,
             playerMaxHp: 1000,
             playerCurrentMp: 1000,
@@ -66,10 +95,94 @@ export default {
             enemyMaxHp: 1000,
             enemyCurrentMp: 1000,
             enemyMaxMp: 1000,
+            playerProgress: [
+                {
+                    img: require("../../assets/ava.png"),
+                    attackAnimation: 0.2,
+                    afterAnimation: 0.455,
+                },
+                {
+                    img: require("../../assets/ava.png"),
+                    attackAnimation: 0.2,
+                    afterAnimation: 0,
+                },
+                {
+                    img: require("../../assets/ava.png"),
+                    attackAnimation: 0.2,
+                    afterAnimation: 0,
+                },
+                {
+                    img: require("../../assets/ava.png"),
+                    attackAnimation: 0.2,
+                    afterAnimation: 0,
+                },
+                {
+                    img: require("../../assets/ava.png"),
+                    attackAnimation: 0.2,
+                    afterAnimation: 0,
+                },
+                {
+                    img: require("../../assets/ava.png"),
+                    attackAnimation: 0.2,
+                    afterAnimation: 0,
+                },
+                {
+                    img: require("../../assets/ava.png"),
+                    attackAnimation: 0.2,
+                    afterAnimation: 0,
+                },
+            ],
+            enemyProgress: [
+                {
+                    img: require("../../assets/ccc.png"),
+                    attackAnimation: 0.1,
+                    afterAnimation: 1,
+                },
+                {
+                    img: require("../../assets/ccc.png"),
+                    attackAnimation: 0.1,
+                    afterAnimation: 0.1,
+                },
+                {
+                    img: require("../../assets/ccc.png"),
+                    attackAnimation: 0.1,
+                    afterAnimation: 0.1,
+                },
+                {
+                    img: require("../../assets/ccc.png"),
+                    attackAnimation: 0.1,
+                    afterAnimation: 0.1,
+                },
+                {
+                    img: require("../../assets/ccc.png"),
+                    attackAnimation: 0.1,
+                    afterAnimation: 0.1,
+                },
+                {
+                    img: require("../../assets/ccc.png"),
+                    attackAnimation: 0.1,
+                    afterAnimation: 0.1,
+                },
+            ],
+            skillPool: [
+                {
+                    img: require("../../assets/ava.png"),
+                    attackAnimation: 0.2,
+                    afterAnimation: 0,
+                },
+                {
+                    img: require("../../assets/bbb.png"),
+                    attackAnimation: 0.3,
+                    afterAnimation: 0.1,
+                },
+            ],
         };
     },
     created() {
         document.querySelector("#loading").style.cssText = "display:none";
+        // this.fightRun = setInterval(() => {
+        //     ++this.runTime;
+        // }, 100);
     },
     methods: {
         fight() {
@@ -77,7 +190,7 @@ export default {
             this.playerCurrentMp -= 100;
             this.enemyCurrentHp -= 100;
             this.enemyCurrentMp -= 50;
-            if (this.playerCurrentHp < 100) {
+            if (this.playerCurrentHp < -1) {
                 this.playerCurrentHp = 1000;
                 this.playerCurrentMp = 1000;
                 this.enemyCurrentHp = 1000;
